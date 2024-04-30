50149f34784501316a8b3c5930abedb038eeb19c
kunalkumar168 <kunal.kumar.eee14@itbhu.ac.in>
2024-04-30 00:15:04 +0000

    All results and modified bash scripts

--- roberta_run.bash
+++ roberta_run.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 GLUE=("cola" "mrpc" "mnli" "sst2" "qqp" "qnli" "rte" "stsb" "wnli")
-MODEL_NAME="roberta-large" #roberta-base
+PRECISIONS=("W4_8A8" "W4_8A16" "W8A8")
+MODELS=("roberta-base" "roberta-large")
 
 # Install Git LFS
 git lfs install
@@ -9,30 +10,38 @@ git lfs install
 # Loop through each dataset and clone
 for task in "${GLUE[@]}"
 do
-    rm -rf ./"$MODEL_NAME-$task"
-    if [ "$MODEL_NAME" = "roberta-base" ]; then
-        if [ "$task" = "mrpc" ]; then
-            git clone https://huggingface.co/Intel/"$MODEL_NAME-$task"
-        elif [ "$task" = "wnli" ]; then
-            git clone https://huggingface.co/JeremiahZ/"$MODEL_NAME-$task"
-        else
-            git clone https://huggingface.co/WillHeld/"$MODEL_NAME-$task"
+    for MODEL_NAME in "${MODELS[@]}"
+    do
+        rm -rf ./"$MODEL_NAME-$task"
+        echo "Currently Running for ${MODEL_NAME}-${task}"
+        echo "--------------------------- START ----------------------------------"
+        if [ "$MODEL_NAME" = "roberta-base" ]; then
+            if [ "$task" = "mrpc" ]; then
+                git clone https://huggingface.co/Intel/"$MODEL_NAME-$task"
+            elif [ "$task" = "wnli" ]; then
+                git clone https://huggingface.co/JeremiahZ/"$MODEL_NAME-$task"
+            else
+                git clone https://huggingface.co/WillHeld/"$MODEL_NAME-$task"
+            fi
+        elif [ "$MODEL_NAME" = "roberta-large" ]; then
+            if [ "$task" = "wnli" ]; then
+                continue
+            else
+                git clone https://huggingface.co/howey/"$MODEL_NAME-$task"
+            fi
         fi
-    elif [ "$MODEL_NAME" = "roberta-large" ]; then
-        if [ "$task" = "wnli" ]; then
-            continue
-        else
-            git clone https://huggingface.co/howey/"$MODEL_NAME-$task"
-        fi
-    fi
-    
-    # Check if roberta-main.py exists and execute it
-    if [ -f "roberta-main.py" ]; then
-        MODEL_CONFIG=$(echo "$MODEL_NAME" | sed 's/-/_/g')
-        python roberta-main.py --model-name $MODEL_NAME --task-name $task --quant-config quant_configs/${MODEL_CONFIG}_config.json
-    else
-        echo "roberta-main.py not found!"
-    fi
-    
-    rm -rf ./"$MODEL_NAME-$task"
+        
+        for precision in "${PRECISIONS[@]}"
+        do
+            # Check if roberta-main.py exists and execute it
+            if [ -f "roberta-main.py" ]; then
+                MODEL_CONFIG=$(echo "$MODEL_NAME" | sed 's/-/_/g')
+                python roberta-main.py --model-name $MODEL_NAME --task-name $task --quant-config quant_configs/${MODEL_CONFIG}_config_${precision}.json
+            else
+                echo "roberta-main.py not found!"
+            fi
+        done
+        echo "--------------------------- FINISH ----------------------------------"
+        rm -rf ./"$MODEL_NAME-$task"
+    done
 done
